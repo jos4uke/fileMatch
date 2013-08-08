668c0aa9c4df53d165fa6325cf2c6e239e13f446
Najla Ben Hassine & Joseph Tran <Joseph.Tran@versailles.inra.fr>
2013-08-08 14:46:48 +0200

    add new version and date in the script header.

--- fileMatch.py
+++ fileMatch.py
@@ -5,7 +5,8 @@
 
 #///////////////////////////////////////////////////////////////////////
 __author__ = "BEN HASSINE Najla(Najla.Ben-Hassine@versailles.inra.fr)"#/
-__version__ = "1.0"				                      			      #/
+__version__ = "1.1"		                      			              #/
+__date__= "20130808"		                      			          #/
 __copyright__ = "Copyright (c) 2013-2014 BHN"                         #/
 __license__ = "GROUPE DEV IJPB"			                      	      #/
 #///////////////////////////////////////////////////////////////////////
